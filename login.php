d48131d3e498dde7fd7b322a8d96df45a2492266
mapc-o <151883869+mapc-o@users.noreply.github.com>
2024-10-03 16:45:40 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<h1>LOGIN</h1>
+<p>Hola mundo login</p>
+
+<!--Pàgina dinàmica-->
+<!--còdigo interpretado de C-->
+<?php
+echo "<h6>Hola</h6>";
+
+$usuario = $_GET['username'];
+$contra = $_GET['password'];
+
+echo "El usuario es: ".$usuario;
+
+?>
